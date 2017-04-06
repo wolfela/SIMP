c9e6eeef97157d26e78a95dc10ada9187c34d802
K1502676 <adrian.abramowski@kcl.ac.uk>
2017-04-06 15:21:55 +0100

    Rewritten from scratch to work without regex and use classes

--- simp.py
+++ simp.py
@@ -1,53 +1,73 @@
 import re
 import operator
 
-operators = { "+": operator.add, "-": operator.sub, "/": operator.truediv, "*": operator.mul}
+operators = {"+": operator.add, "-": operator.sub, "/": operator.truediv, "*": operator.mul}
 
 control = []
 results = []
 memory = dict()
 
 
-def push(array, x):
-    return array.insert(0, x)
+class Expr:
+    num = None
+
+    def __init__(self, num):
+        self.num = num
 
 
-def pop(array):
-    return array.pop(0)
+class ExprOpExpr(Expr):
+    left = None
+    right = None
+
+    def __init__(self, expr1, op, expr2):
+        self.left = expr1
+        self.right = expr2
+        self.op = op
 
 
-def evaluate(op, num1, num2):
-    return operators[op](int(num1), int(num2))
+class Op:
+    op = None
+
+    def __init__(self, opr):
+        self.op = opr
+
+    def eval(self, num1, num2):
+        return operators[self.op](int(num1), int(num2))
 
 
-def checkFront():
-    current = control.pop()
+def run():
+    exp = control.pop()
+    if type(exp) is ExprOpExpr:
+        control.append(exp.op)
+        control.append(exp.left)
+        control.append(exp.right)
 
-    if type(current) is str:
-        patternAll = re.compile('\d* \* \d*|\d* / \d*|\d* \+ \d*|\d* - \d*')
-        patternSign = re.compile('\*|\+|-|/')
-        patternDigit = re.compile('[\d]*')
-        patternValue = re.compile('!')
+    elif type(exp) is Op:
+        results.append(Expr(Op.eval(exp, int(results.pop().num), int(results.pop().num))))
 
-        if patternAll.match(current):
-            digits = list(filter(None, patternDigit.findall(current)))
-            sign = list(filter(None, patternSign.findall(current)))
-            push(control, sign[0])
-            push(control, digits[1])
-            push(control, digits[0])
+    elif type(exp) is Expr:
+        results.append(exp)
 
-        elif patternSign.match(current):
-            push(results, str(evaluate(current, pop(control), pop(control))))
+    if len(control) > 0:
+        run()
 
-        elif patternValue.match(current):
-            push(results, memory.get(current))
-            
-        elif patternDigit.match(current):
-            push(results, current)
+exp1 = Expr(12)
+exp2 = Expr(13)
+op1 = Op("+")
+expr1 = ExprOpExpr(exp1, op1, exp2)
 
-    if (len(control) is not 0):
-        checkFront()
+exp3 = Expr(8)
+exp4 = Expr(9)
+op2 = Op("*")
+exp5 = Expr(7)
+exp6 = Expr(6)
+exprsub1 = ExprOpExpr(exp5, op2, exp6)
+exprsub2 = ExprOpExpr(exp3, op2, exp4)
+expr2 = ExprOpExpr(exprsub1, op2, exp3)
 
-push(control, '9 + 9')
-checkFront()
-print(results)
+control.append(expr2)
+
+run()
+
+for result in results:
+    print(result.num)
